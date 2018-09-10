ab8a58f8b7ccb38bb966993951175671243ad5df
li kinran <likinran@gmail.com>
2018-09-10 11:33:42 +0900

    レイアウト調整完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  get 'users/new'
 
+  root 'home#index'
+  # get  '/help',    to: 'static_pages#help', as: 'helf'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
