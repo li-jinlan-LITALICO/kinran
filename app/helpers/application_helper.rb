89d1837883047ac7dff8c26a9231064f124c7a27
li kinran <likinran@gmail.com>
2018-09-10 16:02:09 +0900

    商品詳細ページのデータを表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(item, size: 350)
+    gravatar_id = Digest::MD5::hexdigest(item.name.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: item.name, class: "gravatar")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'cart_items/index'
+
   get 'tags/create'
 
   get 'items/index'
 
   get 'users/new'
 
-  root 'home#index'
+  resources :items
+  resources :tags  
+  resources :users
   # get  '/help',    to: 'static_pages#help', as: 'helf'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
