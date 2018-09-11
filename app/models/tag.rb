6f07efa5d233c12fe0df21e5f1f27c5bb217d086
li kinran <likinran@gmail.com>
2018-09-11 19:17:46 +0900

    タグモデルのテスト追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,5 @@
 class Tag < ApplicationRecord
   # has_many : tag_items dependent: :destroy
-  before_save { self.name = name.downcase }
   validates :name,  presence: true, uniqueness: { case_sensitive: false },
             length: { minimum: 1 }
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @tag = Tag.new(name: 'aaa')
+  end
+
+  test "tag name should be present" do
+    @tag.name = "   "
+    assert_not @tag.valid?
+  end
+
+  test "tag name should be unique" do
+    duplicate_tag = @tag.dup
+    duplicate_tag.name = @tag.name.upcase
+    @tag.save
+    assert_not duplicate_tag.valid?
+  end
+
+  test "tag name should have a minimum length" do
+    @tag.name = ""
+    assert_not @tag.valid?
+  end
 end
