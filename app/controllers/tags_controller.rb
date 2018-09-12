937e0771a2d49d080aa4c15316e4bf1c750d5787
li kinran <likinran@gmail.com>
2018-09-12 15:33:52 +0900

    タグの商品一覧ページに商品リストのレイアウトを複製

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,9 @@ class TagsController < ApplicationController
   end
 
   def show
+    @tags = Tag.all
     @tag = Tag.new
+    @items = Item.all
+    @items = Item.order('updated_at DESC').paginate(page: params[:page], per_page: 12)
   end
 end
