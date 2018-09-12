0075a589bc264abf122ad0926ce2bc53c10d4b9a
li kinran <likinran@gmail.com>
2018-09-12 15:22:39 +0900

    ホームページの商品リストの配置完了

--- db/seeds/items.rb
+++ db/seeds/items.rb
@@ -1,11 +1,11 @@
-Item.create!(name:                         "tool-0",
-             picture:                  "tool-0.png",
+Item.create!(name:                         "商品-0",
+             picture:                  "商品-0.png",
              per_cost:                         2000,
              description: "1歳児向けのお絵かき玩具です。",
              stock_number:                      200)
 
 99.times do |n|
-  name  = "tool-#{n+1}"
+  name  = "商品-#{n+1}"
   picture = name + ".png"
   per_cost = 1000
   description = "1歳児向けのお絵かき玩具です。"
