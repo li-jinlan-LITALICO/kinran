7e6687471524740ace980dd96a4eeb89f9029a49
li kinran <likinran@gmail.com>
2018-09-12 18:30:24 +0900

    商品のサンプルデータの修正

--- db/seeds/items.rb
+++ db/seeds/items.rb
@@ -7,9 +7,9 @@ Item.create!(name:                         "商品-0",
 99.times do |n|
   name  = "商品-#{n+1}"
   picture = name + ".png"
-  per_cost = 1000
+  per_cost = 1000 + n
   description = "1歳児向けのお絵かき玩具です。"
-  stock_number = 100
+  stock_number = 100 + n
   Item.create!(name:                      name,
                picture:                picture,
                per_cost:              per_cost,
