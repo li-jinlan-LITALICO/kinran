862af21e71ac6204c93c8a85476d3d5490625320
li kinran <likinran@gmail.com>
2018-09-14 11:23:38 +0900

    カート商品のサンプルデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,12 +60,12 @@ followers = items[1..30]
 following.each { |t| item.follow(t) } #2~6のタグにはid1の商品が紐づいている
 followers.each { |i| i.follow(tag) } #id2~31の商品はid1のタグに紐づいている
 
-# カートの商品（ユーザーと商品の関係）データ
-items = Item.all
-item  = items.first
-users = User.all
-user = users.first
-add_cart_items = items[1..5]
-add_cart_users = users[1..3]
-add_cart_items.each { |i| user.add_cart_item(i) } #ユーザー1はid1~5の商品をカートに追加している
-add_cart_users.each { |u| u.add_cart_item(item) } #id2~4のユーザーは、id1の商品をカートに追加している
+# カートの商品データ
+10.times do |n|
+  cart_item_number  = n + 1
+  user_id = n + 1
+  item_id = n + 1
+  CartItem.create!(cart_item_number: cart_item_number,
+                   user_id:                   user_id,
+                   item_id:                   item_id)
+end
